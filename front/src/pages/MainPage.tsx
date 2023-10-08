76ff454e19ade6e7c5cc6a47fd4df9198f57a11c
devchaeyoung <chaemaa00@gmail.com>
2023-10-09 07:20:59 +0900

    Style: 메인페이지 컴포넌트 색상 적용 - chakra-ui 패키지 설치 - favicon.ico 변경

--- front/src/pages/MainPage.tsx
+++ front/src/pages/MainPage.tsx
@@ -1,12 +1,17 @@
+import "../styles/MainPage.css";
+import KakaoButton from "../components/KakaoButton";
+import { Button } from "@chakra-ui/react";
+
 export default function MainPage() {
   return (
     <>
-      <div>
+      <div className="user-join">
         <p>회원가입</p>
         <p>로그인</p>
+        <KakaoButton />
       </div>
-      <div>서비스 소개</div>
-      <p>레벨테스트 하러가기</p>
+      <div className="info">서비스 소개</div>
+      <Button className="test-btn">레벨테스트 하러가기</Button>
     </>
   );
 }
