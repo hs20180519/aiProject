5e0694518552afc9ee40470ef8f467b4016a5dd4
[AI8/1반] 박영현 <chanel0908@gmail.com>
2023-10-10 20:26:35 +0900

    Feat: MainPage Structuring

---
+++ front/src/pages/TestPage.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import WordCard from "../components/WordCard";
+import MoveButton from "../components/MoveButton";
+
+export default function TestPage() {
+  return (
+    <>
+      <WordCard />
+      <WordCard />
+      <WordCard />
+      <WordCard />
+      <WordCard />
+      <WordCard />
+      <MoveButton />
+      <MoveButton />
+    </>
+  );
+}
