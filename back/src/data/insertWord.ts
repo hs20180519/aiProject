f1796dc04a41f6cb24b377baf479899bac5abf8f
[AI8/1반] 정현수 <conjugate0519@gmail.com>
2023-10-19 01:26:39 +0900

    Feat: 영단어 데이터셋 추가

---
+++ back/src/data/insertWord.ts
@@ -0,0 +1,45 @@
+import { PrismaClient } from "@prisma/client";
+import csv from "csv-parser";
+import * as fs from "fs";
+
+const prisma = new PrismaClient();
+
+interface RowType {
+  word: string;
+  meaning: string;
+  category: string;
+}
+
+async function main() {
+  try {
+    // CSV 파일 경로
+    const filePath = "./output2.csv";
+
+    // CSV 파일을 읽어서 데이터베이스에 넣기
+    fs.createReadStream(filePath)
+      .pipe(csv())
+      .on("data", async (row: RowType) => {
+        const { word, meaning, category } = row;
+
+        // Word 모델에 데이터 넣기
+        await prisma.word.create({
+          data: {
+            word,
+            meaning,
+            category,
+          },
+        });
+      })
+      .on("end", () => {
+        console.log("데이터 적재 완료");
+      });
+  } catch (error) {
+    console.error("오류 발생:", error);
+  } finally {
+    await prisma.$disconnect();
+  }
+}
+
+main().catch((e) => {
+  throw e;
+});
