024c6d666a364ae2b1ec58619563c779934a85dc
JoDaeChan <daechan476@gmail.com>
2023-10-20 09:57:02 +0900

    Refactor: 쿼리 유효성검사 타입 변경

--- back/src/validators/validatorQuerySchema.ts
+++ back/src/validators/validatorQuerySchema.ts
@@ -1,12 +1,11 @@
 import Joi from "joi";
 
 export const validatorQuerySchema = Joi.object({
-  correct: Joi.boolean().optional(),
-  incorrect: Joi.boolean().optional(),
-  csat: Joi.boolean().optional(),
-  toeic: Joi.boolean().optional(),
-  toefl: Joi.boolean().optional(),
-  ielts: Joi.boolean().optional(),
-  custom: Joi.boolean().optional(),
+  correct: Joi.string().optional(),
+  incorrect: Joi.string().optional(),
+  csat: Joi.string().optional(),
+  toeic: Joi.string().optional(),
+  toefl: Joi.string().optional(),
+  custom: Joi.string().optional(),
   customBookId: Joi.string().optional(),
 });
