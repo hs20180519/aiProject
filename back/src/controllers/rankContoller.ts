06afddb43f1dd1391fa28871d924ca7e28ae6563
devchaeyoung <chaemaa00@gmail.com>
2023-10-21 21:34:42 +0900

    Docs: rank api 중

--- back/src/controllers/rankContoller.ts
+++ back/src/controllers/rankContoller.ts
@@ -5,13 +5,8 @@ import * as rankService from "../services/rankService";
 // (현재 서버 아키텍쳐가 함수표현식을 사용한다는 전제하에 구현한 상태이고 어차피 정적메서드를 사용한다면 굳이 클래스를 사용할 필요가 있을까요?)
 
 export const getUsersRankList = async (req: Request, res: Response, next: NextFunction) => {
-  try {
-    const rankingChange = await rankService.getUsersRankList;
-    res.json(rankingChange);
-  } catch (error) {
-    console.error("랭킹을 불러올 수 없습니다.", error);
-    res.status(500).json({ error: "랭킹 변화 조회 오류" });
-  }
+  const rankingChange = await rankService.getUsersRankList;
+  return rankingChange;
 };
 
 /** 유저 랭킹 변화 */
