45edb1dda754853c1fe450b4afd82fd17f6b9e72
JoDaeChan <daechan476@gmail.com>
2023-10-21 15:33:50 +0900

    Docs: 채영짱은 보시오

--- back/src/routers/userRouter.ts
+++ back/src/routers/userRouter.ts
@@ -7,7 +7,7 @@ const userRouter: Express = Router();
 
 userRouter.get("/", passportJwt, userController.getProfile);
 
-//todo 이미 rank api에 동일한 기능을 하는 경로가있는데 굳이 초입을 분리할 필요가 있을까요?
+// todo 이미 rank api에 동일한 기능을 하는 경로가있는데 굳이 초입을 분리할 필요가 있을까요?
 
 // 유저 랭킹 차 정보를 가져오는 엔드포인트
 userRouter.get("/rankingChange/:userId", passportJwt, rankController);
