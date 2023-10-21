c290d2efaf7c663c6906b75fcfc6ac29efee2a7d
devchaeyoung <chaemaa00@gmail.com>
2023-10-22 00:40:15 +0900

    Feat: rank수정

--- back/src/controllers/rankContoller.ts
+++ back/src/controllers/rankContoller.ts
@@ -1,6 +1,7 @@
 import { Request, Response, NextFunction } from "express";
 import * as rankService from "../services/rankService";
 import { User, Rank } from "@prisma/client";
+import { UserDto } from "../dtos/userDto";
 
 //todo 코드 스타일을 통일하는게 좋지 않을까요? 함수 표현식으로 통일하거나 클래스를 이용한 정적메서드 방식으로 통일하거나
 // (현재 서버 아키텍쳐가 함수표현식을 사용한다는 전제하에 구현한 상태이고 어차피 정적메서드를 사용한다면 굳이 클래스를 사용할 필요가 있을까요?)
@@ -17,8 +18,8 @@ export const getUsersRankList = async (req: Request, res: Response, next: NextFu
 
 export const getUserRank = async (req: Request, res: Response, next: NextFunction) => {
   try {
-    const user = req.user as Rank;
-    const rank = await rankService.getUserRank(userId);
+    const user = req.user as User;
+    const rank = await rankService.getUserRank(user);
   } catch (e) {
     res.status(400).json(`${e}\n 유저의 랭킹을 가져올 수 없습니다.`);
     next(e);
--- back/src/services/rankService.ts
+++ back/src/services/rankService.ts
@@ -10,9 +10,13 @@ const prisma = new PrismaClient();
 /*
  * 가져올 값
  * 1. 유저 전체 순위 목록 1위~100위 = userRankList
- * 2. 로그인한 유저의 현재 순위 = currentRank
- * 3. 로그인한 유저의 이전 순위 = pastRank
- * 4. 로그인한 유저의 현재순위 - 이전순위 = 순위변화(changedRank)
+ * 2. 로그인한 유저의 오늘 순위 = todayRank
+ * 3. 로그인한 유저의 어제 순위 = yesterdayRank
+ * 4. 로그인한 유저의 오늘순위 - 어제순위 = 순위변화(changedRank)
+ *
+ * 일주일에 한번 매주 월요일 아침 유저 순위 리셋
+ * 하루에 두번 유저랭킹값 저장하여 유저에게 랭킹변화를 보여줌으로 학습 유도
+ *
  */
 
 //todo 반환객체에 DTO 패턴 적용하기
@@ -41,10 +45,22 @@ export const getUserRank = async (user: User) => {
 };
 
 /** 매일 오후 6시 유저랭킹 저장 */
-export const updateUserRank6pm = async (rank: Rank) => {
-  cron.schedule("0 18 * * ,", (rank) => {});
+export const updateUserRank6pm = async (user: User) => {
+  cron.schedule("0 18 * * ,", async (rank) => {
+    const rankList = await getUsersRankList();
+    const userRank = rankList.findIndex((rank) => rank.user.email === user.email);
+    return userRank + 1;
+  });
 };
+
 /** 매일 오전 8시 유저랭킹 저장 */
+export const updateUserRank8am = async (user: User) => {
+  cron.schedule("0 8 * * ,", async (rank) => {
+    const rankList = await getUsersRankList();
+    const userRank = rankList.findIndex((rank) => rank.user.email === user.email);
+    return userRank + 1;
+  });
+};
 
 /** 유저 랭킹차 */
 export const userGapRank = async (user: User) => {
@@ -62,8 +78,3 @@ export const userGapRank = async (user: User) => {
     return gap;
   }
 };
-
-// 0. 점수 증가시키기 전에 현재 등수 저장
-// 1. 점수 증가시킴
-// 2. 현재 등수 구하려면 점수 리스트 정렬
-// 3.
