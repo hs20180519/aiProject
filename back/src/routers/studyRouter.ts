35c68bb767a1aa0b5f3790c9b7f2c3705508c48e
JoDaeChan <daechan476@gmail.com>
2023-10-24 10:35:24 +0900

    Refactor: 학습 저장 API
    body -> query string

--- back/src/routers/studyRouter.ts
+++ back/src/routers/studyRouter.ts
@@ -1,14 +1,13 @@
 import Router, { Express } from "express";
 import * as studyController from "../controllers/studyController";
 import passportJwt from "../middlewares/passportJwt";
-import * as joi from "../validators/studyValidator";
 const studyRouter: Express = Router();
 
 studyRouter.get("/experience", studyController.experience);
 
 studyRouter
   .get("/", passportJwt, studyController.getWords)
-  .post("/", joi.validateSaveLearn, passportJwt, studyController.saveLearn);
+  .post("/", passportJwt, studyController.saveLearn);
 
 studyRouter.get("/result", passportJwt, studyController.getLearnResult);
 export default studyRouter;
