730c3a32e42ede422b9ccf5e353244ff45713036
[AI8/1반] 임재홍 <nerineririn@gmail.com>
2023-10-29 07:29:52 +0900

    Style: 스타일링

--- front/src/pages/TestGptWordPage/ParamGptDialogPage.tsx
+++ front/src/pages/TestGptWordPage/ParamGptDialogPage.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState, useCallback } from "react";
-import { Text, Box, Flex, Spinner, useToast } from "@chakra-ui/react";
+import { Text, Box, Flex, Spinner, useToast, Tag, Center } from "@chakra-ui/react";
 import { FetchGpt } from "../../apis/gpt";
 import ScriptDialog from "./components/ScriptDialog";
 import { useLocation } from "react-router-dom";
@@ -54,9 +54,30 @@ const ParamGptDialogPage = () => {
   return (
     <Flex direction="column" align="center" justify="flex-start" minHeight="100vh">
       <Box background="white" boxShadow="md" p={6} rounded="md" flexGrow={1} width="100%">
+        <Center>
+          <Box m="4">
+            <Text fontSize="xl" fontWeight="bold">
+              대화를 생성할 단어 리스트
+            </Text>
+            <Text mt="2">
+              {Object.keys(receivedWords).map((word, index) => (
+                <Tag
+                  key={index}
+                  size="md"
+                  variant="solid"
+                  colorScheme="teal"
+                  m={1}
+                >
+                  {word}
+                </Tag>
+              ))}
+            </Text>
+          </Box>
+        </Center>
+
         {isScriptLoading ? (
           <Flex justifyContent="center" alignItems="center" height="100%" direction="column">
-            <Text fontSize="xl" fontWeight="bold" mb={4}>
+            <Text fontSize="l" fontWeight="bold" mb={4} mt={10}>
               대화문을 생성 중입니다
             </Text>
             <Spinner />
