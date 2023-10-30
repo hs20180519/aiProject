e93306ef515092cb0d662e73cb86d49c834c24c7
devchaeyoung <chaemaa00@gmail.com>
2023-10-30 18:45:12 +0900

    Feat: Add Custom Page 완성

--- front/src/apis/customWord.ts
+++ front/src/apis/customWord.ts
@@ -1,38 +1,26 @@
+/* eslint-disable */
 import { AxiosError } from "axios";
 import * as Api from "./api";
 
 /** 커스텀 단어장 생성 */
 export async function postCustomNote(data: { title: string }) {
   const url = "/book";
-  try {
-    const res = await Api.post(url, data);
-    return res;
-  } catch (e) {
-    console.error(e);
-  }
+  const res = await Api.post(url, data);
+  return res;
 }
 
 /** 커스텀 단어장 목록 조회 */
 export const getCustomNotes = async () => {
   const url = "/book";
-  try {
-    const res = await Api.get(url);
-
-    return res;
-  } catch (e) {
-    console.error(e);
-  }
+  const res = await Api.get(url);
+  return res;
 };
 
 /** 커스텀 단어장 title 수정 */
-export const putCustomNote = async (data: { title: string }) => {
-  const url = `/book`;
-  try {
-    const res = await Api.put(url, data);
-    return res;
-  } catch (e) {
-    console.error(e as AxiosError);
-  }
+export const putCustomNoteTitle = async (data: { title: string }, note_id: string) => {
+  const url = `/book/?customBookId=${note_id}`;
+  const res = await Api.put(url, data);
+  return res;
 };
 
 /**
@@ -41,25 +29,18 @@ export const putCustomNote = async (data: { title: string }) => {
  */
 export const delCustomNote = async (queryParams: string) => {
   const url = `/book`;
-  try {
-    const queryString = new URLSearchParams(queryParams).toString();
-    const fullUrl = `${url}?${queryString}`;
-    const res = await Api.delete(fullUrl);
-    return res;
-  } catch (e) {
-    console.error(e);
-  }
+  const queryString = new URLSearchParams(queryParams).toString();
+  const fullUrl = `${url}?${queryString}`;
+  console.log(fullUrl);
+  const res = await Api.delete(fullUrl);
+  return res;
 };
 
 /** 커스텀 단어장 전체 삭제 */
 export const delAllCustomNote = async () => {
   const url = `/book`;
-  try {
-    const res = await Api.delete(url);
-    return res;
-  } catch (e) {
-    console.error(e);
-  }
+  const res = await Api.delete(url);
+  return res;
 };
 
 /**
@@ -67,19 +48,15 @@ export const delAllCustomNote = async () => {
  * query?customBookId=id
  * body { word: string, meaning: string }
  */
-export const postCustomWord = async (
+export const postCustomWordAdd = async (
   queryParams: string,
   data: { word: string; meaning: string },
 ) => {
   const url = `/book/word`;
-  try {
-    const queryString = new URLSearchParams(queryParams).toString();
-    const fullUrl = `${url}?${queryString}`;
-    const res = await Api.post(fullUrl, data);
-    return res;
-  } catch (e) {
-    console.error(e);
-  }
+  const queryString = new URLSearchParams(queryParams).toString();
+  const fullUrl = `${url}?${queryString}`;
+  const res = await Api.post(fullUrl, data);
+  return res;
 };
 
 /**
@@ -97,7 +74,10 @@ export const getNoteDetail = async (queryParams: string) => {
   const url = `/book/word`;
   const queryString = new URLSearchParams(queryParams).toString();
   const fullUrl = `${url}?${queryString}`;
+  console.log(fullUrl);
   const res = await Api.get(fullUrl);
+  console.log("---------요청------");
+  console.log(res);
   return res;
 };
 
@@ -113,12 +93,8 @@ export const putCustomWord = async (
   const url = `/book/word`;
   const queryString = new URLSearchParams(queryParams).toString();
   const fullUrl = `${url}?${queryString}`;
-  try {
-    const res = await Api.put(fullUrl, data);
-    return res;
-  } catch (e) {
-    console.error(e);
-  }
+  const res = await Api.put(fullUrl, data);
+  return res;
 };
 
 /**
@@ -129,10 +105,6 @@ export const delCustomWord = async (queryParams: string) => {
   const url = `/book/word`;
   const queryString = new URLSearchParams(queryParams).toString();
   const fullUrl = `${url}?${queryString}`;
-  try {
-    const res = await Api.delete(fullUrl);
-    return res;
-  } catch (e) {
-    console.log(e);
-  }
+  const res = await Api.delete(fullUrl);
+  return res;
 };
