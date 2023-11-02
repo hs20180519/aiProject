0e56391e12ff104704bdf56c43f8dfb97459037c
정현수 <conjugate0519@gmail.com>
2023-11-02 16:46:58 +0900

    Style: storageWordBox

--- front/src/pages/Storage/Components/StorageWordBox.tsx
+++ front/src/pages/Storage/Components/StorageWordBox.tsx
@@ -5,33 +5,26 @@ const StorageWordBox = ({ word, onBookmarkClick }) => {
   return (
     <Flex
       width="100%"
-      justifyContent="space-between"
       style={{
-        textAlign: "center",
         backgroundColor: "white",
         height: "100px",
         border: "1px solid #ddd",
         borderRadius: "8px",
-        paddingLeft: "10px",
-        paddingRight: "3px",
-        marginBottom: "10px",
-        transition: "background-color 0.3s ease", // Hover 시 색상 변경 애니메이션
-      }}
-      _hover={{
-        backgroundColor: "teal.100", // Hover 상태에서 색상 변경
+        flexDirection: "column",
       }}
     >
-      <Box flex={1} style={{ display: "flex", flexDirection: "column", justifyContent: "center" }}>
-        <p style={{ fontSize: "17px", fontWeight: "bold" }}>{word.word}</p>
-        <p style={{ color: "gray" }}>{word.meaning}</p>
-      </Box>
-      <Box>
+      <Box width="100%" textAlign="end">
         <BookMark
           favorite={word.isFavorite}
           onClick={() => {
             onBookmarkClick(word.id, word.isFavorite);
           }}
         />
+      </Box>
+
+      <Box width="100%" textAlign="center">
+        <p style={{ fontSize: "17px", fontWeight: "bold" }}>{word.word}</p>
+        <p style={{ color: "gray" }}>{word.meaning}</p>
       </Box>
     </Flex>
   );
