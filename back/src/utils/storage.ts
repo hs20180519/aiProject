4e2cfee5db8c8bed7ab086c34b25bb2720e4bb1c
JoDaeChan <daechan476@gmail.com>
2023-11-04 00:49:16 +0900

    Refactor: upload path update

--- back/src/utils/storage.ts
+++ back/src/utils/storage.ts
@@ -3,7 +3,7 @@ import { v4 as uuidv4 } from "uuid";
 import path from "path";
 
 const storage = multer.diskStorage({
-  destination: path.join(__dirname, "../../public/images"),
+  destination: path.join(__dirname, "../../public/image"),
   filename: (req, file: Express.Multer.File, callback): void => {
     const imageFormat: string = file.mimetype.split("/")[1];
     const filename: string = `img_${uuidv4()}.${imageFormat}`;
